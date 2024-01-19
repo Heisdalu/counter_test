efa72796301d76efcf70784d7451b05b66a7511b
divine <divineobi07@gmail.com>
2024-01-19 18:24:04 +0100

    chore; edit config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,14 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: {
+        1: "1px",
+      },
+      borderColor: {
+        1: "#000",
+      },
+    },
   },
   plugins: [],
 };
